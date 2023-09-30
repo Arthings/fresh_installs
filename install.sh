f02945fd18024ce5770f6c95f5e4e9433c353704
Arthings <fantosaw@gmail.com>
2023-09-30 13:49:17 +0200

    add first fresh install script nature specific to deb distros

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo apt install neofetch r-base python3-pip python3-dev git
+sudo apt install curl
+
+curl -sS https://starship.rs/install.sh | sh
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt install steam 
+
+
+
